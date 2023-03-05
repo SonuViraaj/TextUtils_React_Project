adaf274db16142d9928b4b609a8288bae06f512b
Sonu Viraaj <sonukumar5845@gmail.com>
2023-03-05 18:23:57 +0530

    Updates

---
+++ precache-manifest.8727ec17fd36eccce25c467c8b955394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "feb6dd8dd25e73194cff",
+    "url": "/TextUtils_React_Project/static/css/main.a5891868.chunk.css"
+  },
+  {
+    "revision": "feb6dd8dd25e73194cff",
+    "url": "/TextUtils_React_Project/static/js/main.feb6dd8d.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/TextUtils_React_Project/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "596c5b9fd104e61ff086",
+    "url": "/TextUtils_React_Project/static/js/2.596c5b9f.chunk.js"
+  },
+  {
+    "revision": "6df150557802b5d64a43",
+    "url": "/TextUtils_React_Project/static/js/runtime~main.6df15055.js"
+  },
+  {
+    "revision": "5786e77e1ccf12acbd26f3abd50bca80",
+    "url": "/TextUtils_React_Project/index.html"
+  }
+];
